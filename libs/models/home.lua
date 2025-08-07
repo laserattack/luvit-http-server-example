34bc6480f9459fdd7c4096edbadf122c67b0369a
serr <sc7227484@gmail.com>
2025-08-07 12:32:50 +0300

    some changes

--- libs/models/home.lua
+++ libs/models/home.lua
@@ -6,8 +6,11 @@ local render = require("templater").render ---@type function
 
 local function createPage()
     local template = assert(fs.readFile("home.html"))
+    local cached_at_unix = os.time()
+    local cached_at_date = os.date("%c", cached_at_unix)
     local page = render(template, {
-        version = os.time()
+        version = cached_at_unix,
+        cached_at = cached_at_date
     })
     return page
 end
