819272d5ae3a6f9fb8eb0dc4a5142e0149482e06
serr <sc7227484@gmail.com>
2025-08-07 20:31:10 +0300

    some changes

--- libs/models/home.lua
+++ libs/models/home.lua
@@ -7,24 +7,35 @@ local render = require("templater").render
 
 local mdToHtml = require("markdown")
 
+local function loadMdParts(dir)
+    local parts = {}
+    for filename in io.popen('ls '..dir..'/*.md 2>/dev/null'):lines() do
+        local num = filename:match("/(%d+)%.md$")
+        if num then
+            parts[num] = mdToHtml(assert(fs.readFile(filename)))
+        end
+    end
+    return parts
+end
+
 local function createPage()
     local template = assert(fs.readFile("templates/home.html"))
     local cached_at_unix = os.time()
     local cached_at_date = os.date("%c", cached_at_unix)
 
-    -- md parts
-    local firstPart = mdToHtml(assert(fs.readFile("parts/first.md")))
-    local secondPart = mdToHtml(assert(fs.readFile("parts/second.md")))
-    local thirdPart = mdToHtml(assert(fs.readFile("parts/third.md")))
-
-    local page = render(template, {
+    local pageParams = {
         version = cached_at_unix,
         cached_at = cached_at_date,
-        -- md parts
-        firstPart = firstPart,
-        secondPart = secondPart,
-        thirdPart = thirdPart
-    })
+    }
+
+    -- Загружает все файлы вида <число>.md из директории parts
+    local mdParts = loadMdParts("parts")
+    for k, v in pairs(mdParts) do
+        pageParams[k] = v
+    end
+
+    local page = render(template, pageParams)
+
     return page
 end
 
