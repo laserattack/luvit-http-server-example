623083140bd015afe91de4b83da94b81bc16b034
serr <sc7227484@gmail.com>
2025-08-08 14:51:08 +0300

    some changes

--- libs/models/home.lua
+++ libs/models/home.lua
@@ -1,5 +1,4 @@
--- Тут задается директория, относительно который ищутся views
-local fs = require('coro-fs')
+local fst = require('fs-tools')
 
 -- Функция рендера шаблона
 ---@type function
@@ -12,14 +11,14 @@ local function loadMdParts(dir)
     for filename in io.popen('ls '..dir..'/*.md 2>/dev/null'):lines() do
         local num = filename:match("/(%d+)%.md$")
         if num then
-            parts[num] = mdToHtml(assert(fs.readFile(filename)))
+            parts[num] = mdToHtml(assert(fst.readFile(filename)))
         end
     end
     return parts
 end
 
 local function createPage()
-    local template = assert(fs.readFile("templates/home.html"))
+    local template = assert(fst.readFile("templates/home.html"))
     local cachedAtUnix = os.time()
     local cachedAtDate = os.date("%c", cachedAtUnix)
 
